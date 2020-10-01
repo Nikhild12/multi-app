1ad136870a97f3bbda5e3389401256076b217683
mayur tikundi <mayurtikundi12@gmail.com>
2020-10-01 15:50:15 +0530

    first commit

--- projects/app1/src/app/app.module.ts
+++ projects/app1/src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { App1SharedModule } from 'projects/app1/src/app/app.module';
-import { App2SharedModule } from 'projects/app2/src/app/app.module';
+import { View1Component } from './view1/view1.component';
+import { View2Component } from './view2/view2.component';
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    View1Component,
+    View2Component,
     NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    App1SharedModule.forRoot(),
-    App2SharedModule.forRoot()
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+@NgModule({})
+export class App1SharedModule{
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: AppModule,
+      providers: []
+    }
+  }
+}
